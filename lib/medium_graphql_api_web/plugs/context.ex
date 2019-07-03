c72eec78aaca46a79b85142a3122dc1dd60d281a
wolfiton <dragutadan@gmail.com>
2019-07-03 13:46:13 +0300

    finished context and modified guardian.ex

--- lib/medium_graphql_api_web/plugs/context.ex
+++ lib/medium_graphql_api_web/plugs/context.ex
@@ -11,6 +11,12 @@ defmodule MediumGraphqlApiWeb.Plugs.Context do
   end
 
   defp build_context(conn) do
-    ["Bearer"] <- get_req_header(conn, "authorization")
+    with ["Bearer " <> token] <- get_req_header(conn, "authorization"),
+         {:ok, claims} <- MediumGraphqlApi.Guardian.decode_and_verify(token),
+         {:ok, user} <- MediumGraphqlApi.Guardian.resource_from_claims(claims) do
+      %{current_user: user}
+    else
+      _ -> %{}
+    end
   end
 end
