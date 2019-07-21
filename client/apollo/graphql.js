71a6db923191825a47e7359c73b23fba918ec2c4
FelisOrion <darkyfelis@gmail.com>
2019-07-21 19:30:13 +0200

    added few querys

---
+++ client/apollo/graphql.js
@@ -0,0 +1,36 @@
+import gql from 'graphql-tag'
+
+export const SIGNIN_MUTATION = gql `
+    mutation SignIn($input: SessionInput!) {
+        loginUser(input: $input) {
+            token
+            user {
+                id
+            }
+        }
+    }
+`
+
+export const SIGNUP_MUTATION = gql `
+    mutation SignUp($input: UserInput!) {
+        registerUser(input: $input) {
+            id
+        }
+    }
+`
+
+export const POSTS_QUERY = gql `
+    query Posts {
+        posts {
+            id
+        }
+    }
+`
+
+export const POST_QUERY = gql `
+    query Post ($id: Int!) {
+        post(id: $id) {
+            id
+        }
+    }
+`
