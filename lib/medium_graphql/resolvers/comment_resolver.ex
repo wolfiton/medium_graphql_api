b70d257d0d28534dad3f8e77c080a7ecb72fa052
wolfiton <dragutadan@gmail.com>
2019-07-23 17:04:33 +0300

    Final version clean start

--- lib/medium_graphql/resolvers/comment_resolver.ex
+++
@@ -1,8 +0,0 @@
-defmodule MediumGraphqlApiWeb.Schema.CommentResolver do
-
-  def create_comment(_, %{input: input}, %{context: %{current_user: current_user}}) do
-    comment_input = Map.merge(input, %{user_id: current_user.id})
-    IO.inspect(comment_input)
-    Medium.create_comment(comment_input)
-  end
-end
