e2ff58d828cb8c23837d39fe26c179a237f296a8
FelisOrion <darkyfelis@gmail.com>
2019-10-22 16:02:49 +0200

    updated to last version

--- config/dev.exs
+++ config/dev.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 # Configure your database
 config :medium_graphql_api, MediumGraphqlApi.Repo,
-  username: "dan",
+  username: "postgres",
   password: "postgres",
   database: "medium_graphql_dev",
   hostname: "localhost",
--- priv/repo/migrations/20190725110952_create_users.exs
+++ priv/repo/migrations/20190725110952_create_users.exs
@@ -7,12 +7,18 @@ defmodule MediumGraphqlApi.Repo.Migrations.CreateUsers do
       add :email, :string
       add :password, :string
       add :role, :string
-      add :confirmed, :string
+      add :confirmed, :boolean
       add :attempts, :string
-      add :locked, :string
+      add :locked, :boolean
 
       timestamps()
     end
 
+    execute("CREATE INDEX index_users_email_trgm_idx ON users USING gin (email gin_trgm_ops);")
+    execute("CREATE INDEX index_users_username_trgm_idx ON users USING gin (username gin_trgm_ops);")
+    execute("CREATE INDEX index_users_on_confirmed_idx ON users(confirmed) WHERE confirmed = true")
+    execute("CREATE INDEX index_users_on_locked_idx ON users(locked) WHERE locked = true")
+    create unique_index(:users, [:email])
+    create unique_index(:users, [:username])
   end
 end
